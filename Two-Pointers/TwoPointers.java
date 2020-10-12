00edcd88e3faf71d7b14cb707298a56616ccfab7
MrinallU <61529929+MrinallU@users.noreply.github.com>
2020-10-11 21:44:21 -0400

    Update TwoPointers.java

--- Two-Pointers/TwoPointers.java
+++ Two-Pointers/TwoPointers.java
@@ -1,5 +1,7 @@
 /*Given a array of Integers find a pair that sums to a given number x
 O(N) rather than O(N^2) from brute force
+
+Make sure the array is sorted for the msot part it should be sorted some exceptions I don't know about.
 */
 int left = 0; int right = 0; int sum = arr[0];
 for(right = 0; right < n; right++){
