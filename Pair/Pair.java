b80edf8ceeae96b5da885655bf1b981cdec592c3
MrinallU <61529929+MrinallU@users.noreply.github.com>
2021-07-01 10:06:01 -0400

    Update Pair.java

--- Pair/Pair.java
+++ Pair/Pair.java
@@ -1,38 +1,38 @@
-public static class pair {
+    public static class pair {
 
-    private final int x;
-    private final int y;
+        private final int x;
+        private final int y;
 
-    public pair(final int x, final int y) {
-        this.x = x;
-        this.y = y;
-    }
+        public pair(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof pair)) {
+                return false;
+            }
+
+            final pair pair = (pair) o;
+
+            if (x != pair.x) {
+                return false;
+            }
+            if (y != pair.y) {
+                return false;
+            }
+
             return true;
         }
-        if (!(o instanceof Pair)) {
-            return false;
-        }
 
-        final Pair pair = (Pair) o;
-
-        if (x != pair.x) {
-            return false;
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
         }
-        if (y != pair.y) {
-            return false;
-        }
-
-        return true;
     }
-
-    @Override
-    public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
-    }
-}
